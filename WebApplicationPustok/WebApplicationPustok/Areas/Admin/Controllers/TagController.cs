54c8e9618b1359e9d9df8fc63a83a9ab1f8e14b4
MelekAzizova <147470006+MelekAzizova@users.noreply.github.com>
2023-12-14 11:49:26 -0800

    Product Details

---
+++ WebApplicationPustok/WebApplicationPustok/Areas/Admin/Controllers/TagController.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationPustok.Context;
+using WebApplicationPustok.Helpers;
+using WebApplicationPustok.Models;
+using WebApplicationPustok.ViewModel.SliderVM;
+using WebApplicationPustok.ViewModel.TagVM;
+
+namespace WebApplicationPustok.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TagController : Controller
+    {
+        public TagController(PustokDbContext dp)
+        {
+            _dp = dp;
+        }
+
+        PustokDbContext _dp {  get; set; }  
+
+        public async  Task<IActionResult> Index()
+        {
+            var items = await _dp.Tags.Select(s => new TagListItem
+            {
+                Id = s.Id,
+                Title = s.Title,
+                Price = s.Price,
+                Brand = s.Brand,
+                ProductCode = s.ProductCode,
+                Stock = s.Stock,
+                ImageUrl = s.ImageUrl,
+                Description = s.Description,
+
+            }).ToListAsync();
+            return View(items);
+           
+        }
+        public IActionResult Create()
+        {
+
+            return View();
+
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Create(TagCreateVM vm)
+        {
+            if (vm.ImgFile != null)
+            {
+                if (!vm.ImgFile.IsCorrectType())
+                {
+                    ModelState.AddModelError("ImageFile", "Wrong file type");
+                }
+                if (!vm.ImgFile.IsValidSize(20f))
+                {
+                    ModelState.AddModelError("ImgFile", "Wrong file size");
+                }
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+
+            Tag tag = new Tag
+            {
+                Title = vm.Title,
+                Description = vm.Description,
+                ImageUrl = await vm.ImgFile.SaveAsync(PathConstants.Product),
+                ProductCode =vm.ProductCode,
+                Price=vm.Price,
+                Stock=vm.Stock,
+                Brand=vm.Brand
+                
+
+
+            };
+            await _dp.Tags.AddAsync(tag);
+            await _dp.SaveChangesAsync();
+            return View();
+
+
+        }
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null || id <= 0) return BadRequest();
+
+            var data = await _dp.Tags.FindAsync(id);
+            if (data == null) return NotFound();
+            return View(new TagUpdateVM
+            {
+                Id=data.Id,
+                Title = data.Title,
+                Description = data.Description,
+                ImageUrl = data.ImageUrl,
+                Price=data.Price,
+                Stock=data.Stock,
+                Brand=data.Brand,
+                ProductCode=data.ProductCode
+
+            });
+
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> Update(int? id, TagUpdateVM vm)
+        {
+            if (id == null || id <= 0) return BadRequest();
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            var data = await _dp.Tags.FindAsync(id);
+            if (data == null) return NotFound();
+            data.Id = vm.Id;
+            data.Title = vm.Title;
+            data.Description = vm.Description;
+            data.ImageUrl = vm.ImageUrl;
+            data.Brand=vm.Brand;
+            data.Stock=vm.Stock;
+            data.ProductCode=vm.ProductCode;
+            data.Price=vm.Price;
+            await _dp.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+
+
+
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+
+            if (id == null) return BadRequest();
+
+            var data = await _dp.Tags.FindAsync(id);
+            if (data == null) return NotFound();
+            _dp.Tags.Remove(data);
+            await _dp.SaveChangesAsync();
+            //TempData["Response"] = new
+            //{
+            //    Icon = "Success",
+            //    Title = "Data deleted succesfully"
+
+            //};
+            return RedirectToAction(nameof(Index));
+
+        }
+    }
+}
---
+++ WebApplicationPustok/WebApplicationPustok/Context/PustokDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using WebApplicationPustok.Models;
+
+namespace WebApplicationPustok.Context
+{
+    public class PustokDbContext:DbContext
+    {
+        
+
+        PustokDbContext _context {  get;  }
+        public PustokDbContext(DbContextOptions<PustokDbContext> opt) : base(opt) { }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<TagProduct> TagsProduct { get; set; }
+        public DbSet<ProductImages> ProductImages { get; set; }
+        public DbSet<Setting> Settings { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Setting>()
+                .HasData(new Setting
+                {
+                    Address = "Baku, Ayna Sultanova st. 221",
+                    Email = "melek.azizova47@gmail.com",
+                    Number = "+994707094535",
+                  
+                    Logo = "assets/img/logo.png",
+                    AccountIcon = "<i class='fa fa-user-o'></i>",
+                    Id = 1
+                });
+            base.OnModelCreating(modelBuilder);
+        }
+        
+
+
+    }
+}
